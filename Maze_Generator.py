1b27e13265ee2e459658f8fedfde6311b0ce37d9
Blgr-Fish <ijjaziad@gmail.com>
2022-03-16 23:03:42 +0100

    Update Maze_Generator.py

--- Maze_Generator.py
+++ Maze_Generator.py
@@ -31,14 +31,12 @@ class Graphe_dictionnaire:
         if y not in self.A.keys():
             self.ajouter_sommet(y)    # on vérifie que le noeud existe ou pas dans le graphe
             
-        if x  not in y and y not in x :
-        
-        
-            if x != y:                    # pour pas qu'on puisse faire une arrete avec le meme noeud
-                self.A[x].append(y)
+
+        if x != y:                    # pour pas qu'on puisse faire une arrete avec le meme noeud
+            self.A[x].append(y)
            
-                if x not in self.A[y]:    # pour les graphes non orientés 
-                    self.A[y].append(x)   
+                #if x not in self.A[y]:    # pour les graphes non orientés 
+                #    self.A[y].append(x)   
 
     """
     def creer_graphe4x4(self):        # créer un labyrinthe un graphe en 4x4 avec 16 noeuds isolés
@@ -129,13 +127,13 @@ class Graphe_dictionnaire:
 
 G = Graphe_dictionnaire()
 
-G.creer_graphe_nxn(2)
+G.creer_graphe_nxn(4)
 
 
 
-#print(G.fusion_aleatoire())
+print(G.fusion_aleatoire())
 #print()
-#print(G.__repr__())
+print(G.__repr__())
 
 
 #     0  1  2  3
