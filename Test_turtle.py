827b8ae94b1cdfa2906549742222799ab5522536
Blgr-Fish <ijjaziad@gmail.com>
2022-03-18 14:07:15 +0100

    Test_TurtleV1.1

--- Test_turtle.py
+++ Test_turtle.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-from Maze_Generator import *
 from math import *
-
+#from Maze_Generator import *
 
 from turtle import *
 
 def turtle_truc(n,G):
     setworldcoordinates(0, 0, n, n)
-    speed(5) 
+    speed(100) 
     maze_completed = False
     
     etape = 0
@@ -20,7 +18,7 @@ def turtle_truc(n,G):
             pd()
             forward(longueur)
             left(90)
-            pu()
+            
   
         
         
@@ -30,50 +28,49 @@ def turtle_truc(n,G):
              
                 
 
-            while etape == 1 : # on créé les parois intérieurs du labyrinthe
+        while etape == 1 : # on créé les parois intérieurs du labyrinthe
+            for i in range(int(n)):
+                setx(0)
+                sety(0)
+                setheading(0)
+                forward(i)
+                left(90)
+                forward(n)
+            for j in range (int(n)) :
+                setx(0)
+                sety(0)
+                setheading(90)
+                forward(j)
+                right(90)
+                forward(n)
+            etape += 1
+                   
                
-               for i in range(len(G)):
-                   for j in range(len(G)):
-                       if key_list[i] in val_list[j]: # on vérifie si un noeud est présent dans les voisins du noeud à côté
-                           print(key_list[i] ,"est relié à ",key_list[j])
-                           
-                           
-                       else :
-                           if key_list[i] != key_list[j]:
-                               if key_list[i][0] == key_list[j][0] : # on vérifie que les y sont les mêmes
-                                   if key_list[i][1] - key_list[j][1] == abs(1) : # et on s'assure que x - x soit = 1
-                                     print(key_list[i] ,"est  pas relié à ",key_list[j])
-                                     pencolor("green")
-                               
-                                     setx(key_list[i][1])
-                                     pd()
-                                     if key_list[i][1] == n-1 :
-                                        sety(key_list[i][0])
-                                        
-                                     else : 
-                                        sety(key_list[i+1][0])
-                                     pu()
+        while etape == 2 :
+            for i in range(len(G)):
+                for j in range(len(G)):
+                    if key_list[i] in val_list[j]:
+                        pencolor('white')
+                        
+                        if key_list[i][0] != 0 :
+                            pu()
+                            setpos(key_list[i][0],key_list[i][1])
+                            pd()
+                            setpos(key_list[j][0],key_list[j][1])
+                            pu()
 
-                                   
-                                   
-                               elif key_list[i][1] == key_list[j][1]: # si les y sont differents
-                                   if key_list[i][0] - key_list[j][0] == abs(1) :
-                                   
-                                     print(key_list[i] ,"est  pas relié à ",key_list[j])
-                                     pencolor("red")
-                                     
-                                     sety(key_list[i][0])
-                                    
-                                     pd()
-                                     if key_list[i][1] == n-1 :
-                                         setx(key_list[i-1][1])
-                                     else:
-                                         setx(key_list[i+1][1])
-                                     pu()
+                        print(key_list[i],"est relié à ", key_list[j])
+                        
+                
+            
+            etape += 1
 
-               etape +=1
-               pass
-
+            while etape == 2 :
+                etape +=1
+                pass
+ 
+            
+        
                         
         break
     done()
@@ -83,16 +80,18 @@ G = {(0, 0): [(1, 0), (0, 1)], (0, 1): [(0, 2)], (0, 2): [(0, 3), (0, 1), (0, 3)
 (0, 2)], (1, 3): [(0, 3), (1, 2)], (2, 0): [(3, 0)], (2, 1): [(2, 2), (2, 0), (3, 1)], (2, 2): [(1, 2), (2, 3)], (2, 3): [(2, 2), (1, 3), (2, 2), (2, 2), (1, 3)], (3, 0): [(3, 1), (3, 1)], (3, 1): [(2, 1)], (3, 2): [(3, 3), (3, 1), 
 (3, 3)], (3, 3): [(2, 3), (2, 3)]}
 
-F = {(0, 0): [(1, 0), (0, 1)], (0, 1): [(0, 0), (1, 1)], (1, 0): [(0, 0)], (1, 1): [(0, 1)]}           
-
+F = {(0, 0): [], (0, 1): [(1, 1)], (1, 0): [], (1, 1): [(0, 1)]}           
 
 E = {(0, 0): [(0, 1)], (0, 1): [(0, 2), (0, 0), (1, 1)], (0, 2): [(1, 2), (0, 1), (0, 1)], (1, 0): [(2, 0), (2, 0)], (1, 1): [(0, 1), (1, 2), (1, 2)], (1, 2): [(0, 2), (2, 2), (1, 1), (0, 2)], (2, 0): [(2, 1), (2, 1), (1, 0)], (2, 1): [(2, 0), (2, 2)], (2, 2): [(2, 1), (1, 2), (1, 2)]}
 
-print(turtle_truc(sqrt(len(G)),G))
+B = {(0, 0): [(1, 0)], (0, 1): [(0, 2)], (0, 2): [(0, 3), (1, 2), (0, 1)], (0, 3): [(0, 2), (1, 3), (0, 4)], (0, 4): [(0, 3), (1, 4)], (1, 0): [(0, 0), (2, 0), (1, 1)], (1, 1): [(2, 1), (1, 2), (1, 0)], (1, 2): [(0, 2), (1, 3), (1, 1)], (1, 3): [(0, 3), (1, 2), (2, 3), (1, 4)], (1, 4): [(2, 4), (0, 4), (1, 3)], (2, 0): [(2, 1), (1, 0), (3, 0)], (2, 1): [(2, 0), (2, 2), (1, 1), (3, 1)], (2, 2): [(3, 2), (2, 1)], (2, 3): [(3, 3), (2, 4), (1, 3)], (2, 4): [(1, 4), (2, 3), (3, 4)], (3, 0): [(4, 0), (2, 0), (3, 1)], (3, 1): [(3, 2), (4, 1), (2, 1), (3, 0)], (3, 2): [(2, 2), (4, 2), 
+(3, 3), (3, 1)], (3, 3): [(2, 3), (4, 3), (3, 2), (3, 4)], (3, 4): [(2, 4), (3, 3)], (4, 0): [(3, 0), (4, 1)], (4, 1): [(4, 0), (3, 1)], (4, 2): [(4, 3), (3, 2)], (4, 3): [(4, 2), (3, 3), (4, 4)], (4, 4): [(4, 3)]} 
+print(turtle_truc(sqrt(len(E)),E))
 
-A = Graphe_dictionnaire()
-A.creer_graphe_nxn(2)
+"""A = Graphe_dictionnaire()
+A.creer_graphe_nxn(10)
+A.fusion_aleatoire()"""
 
-    
+E = {(0, 0): [(0, 1)], (0, 1): [(0, 2), (0, 0), (1, 1)], (0, 2): [(1, 2), (0, 1), (0, 1)], (1, 0): [(2, 0), (2, 0)], (1, 1): [(0, 1), (1, 2), (1, 2)], (1, 2): [(0, 2), (2, 2), (1, 1), (0, 2)], (2, 0): [(2, 1), (2, 1), (1, 0)], (2, 1): [(2, 0), (2, 2)], (2, 2): [(2, 1), (1, 2), (1, 2)]}
 
-    
+#print(turtle_truc(sqrt(a),a))
