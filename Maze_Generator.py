d22971d26d9f035ac7a5ea95e3e552e3685bae57
Blgr-Fish <ijjaziad@gmail.com>
2022-03-18 14:07:19 +0100

    Update Maze_Generator.py

--- Maze_Generator.py
+++ Maze_Generator.py
@@ -35,8 +35,8 @@ class Graphe_dictionnaire:
         if x != y:                    # pour pas qu'on puisse faire une arrete avec le meme noeud
             self.A[x].append(y)
            
-                #if x not in self.A[y]:    # pour les graphes non orientés 
-                #    self.A[y].append(x)   
+            if x not in self.A[y]:    # pour les graphes non orientés 
+                self.A[y].append(x)   
 
     """
     def creer_graphe4x4(self):        # créer un labyrinthe un graphe en 4x4 avec 16 noeuds isolés
