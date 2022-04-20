c11e248db13cbbd88870a8ea5284f0257d398522
Blgr-Fish <ijjaziad@gmail.com>
2022-04-20 11:18:47 +0200

    Update Test_turtle.py

--- Test_turtle.py
+++ Test_turtle.py
@@ -126,6 +126,7 @@ def turtle_lab(n,m,G):
 
             while etape == 4 :
                 print('Maze Completed')
+                hideturtle()
                 maze_completed = True
                 break
 
@@ -138,12 +139,14 @@ cote54 = {(0,0):[(1,0)],(0,1):[(0,2)],(0,2):[(0,1),(0,3)],(0,3):[(0,2),(1,3),(0,
           (2,0):[(3,0),(2,1)],(2,1):[(2,0),(2,2)],(2,2):[(2,1),(1,2),(3,2)],(2,3):[(2,4)],(2,4):[(1,4),(3,4)],
           (3,0):[(2,0),(3,1)],(3,1):[(3,0)],(3,2):[(2,2),(3,3)],(3,3):[(3,2)],(3,4):[(2,4)]}
 
-long = int(input('Entrez la longeur du labyrinthe : '))
+cote3x3 = {(0, 0) :[(0, 1), (1, 0)]  , (0, 1)  :[ (0, 0), (0, 2) ] , (0, 2)  :[ (0,1), (1,2) ] , (1, 0)  :[ (0,0), (1,1), (2,0)] , (1, 1)  :[ (1,0) ]  , (1, 2)  :[ (0,2), (2,2)] , (2, 0)  : [(1,0), (2,1)] , (2, 1)  :[ (2,0)] , (2, 2)  :[ (1,1)] }
+
+"""long = int(input('Entrez la longeur du labyrinthe : '))
 larg = int(input('Entrez la largeur du labyrinthe : '))
 
-graphe = creer_lab_Aldous_frerot(long,larg)
+graphe = creer_lab_Aldous_frerot(long,larg)"""
 
-print(turtle_lab(long,larg,graphe))
+print(turtle_lab(3,3,cote3x3))
 
 
 
